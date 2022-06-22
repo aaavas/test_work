9a03efe5b67295fe7ef25ca08ea912784d41cded
Aaron Avaste <aaronavaste@gmail.com>
2022-06-22 14:08:59 +0300

    Added primitive pages

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse("Hello World")
+class HomePageView(TemplateView):
+    template_name = "avaleht.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
