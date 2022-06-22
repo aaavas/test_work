a56bbac1732de101f2303e0681a25f44ea4d1fa8
Aaron Avaste <aaronavaste@gmail.com>
2022-06-22 15:33:42 +0300

    added tests for db included

--- pages/tests.py
+++ pages/tests.py
@@ -1,40 +1,34 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
+from .models import OsauhinguAndmed
 
 # Create your tests here.
+class OsauhinguAndmedTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.OAandmed = OsauhinguAndmed.objects.create(nimi="Test", registrikood=1001005)
 
+    def test_model_content(self):
+        self.assertEqual(self.OAandmed.nimi, "Test")
+        self.assertEqual(self.OAandmed.registrikood, 1001005)
 
-class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
+    def test_home_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_url_available_by_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "avaleht.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("home"))
         self.assertContains(response, "<h1>Homepage</h1>")
+        self.assertContains(response, "Test")
 
-
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
+    def test_about_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):
+    def test_about_page(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("about"))
         self.assertTemplateUsed(response, "about.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("about"))
         self.assertContains(response, "<h1>Infot leheküljest</h1>")
