dc5eddcbbf077f2e4dbe35322f648b5546a28b7f
Aaron Avaste <aaronavaste@gmail.com>
2022-06-25 18:41:35 +0300

    + osaühingu andmete vaade + lehtede vahel liikumine

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.generic import TemplateView, ListView
+from django.views.generic.base import RedirectView
 from pages.forms import OsauhinguAndmedForm
 from pages.models import OsauhinguAndmedModel
 from django.db.models import Q
@@ -28,7 +30,7 @@ class SearchResultsView(ListView):
         )
 
 
-def FormPageView(request):
+def form_page_view(request):
     context = {}
     form = OsauhinguAndmedForm(request.POST or None)
     if form.is_valid():
@@ -37,3 +39,10 @@ def FormPageView(request):
         return redirect(reverse("home"))
     context["form"] = form
     return render(request, "form.html", context)
+
+
+def ou_profile_view(request, registrikood):
+    context = {}
+    osauhing = get_object_or_404(OsauhinguAndmedModel, registrikood=registrikood)
+    context["ou"] = osauhing
+    return render(request, "profile.html", context)
